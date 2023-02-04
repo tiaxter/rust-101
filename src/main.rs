38d0b91d2e0b730da23f34843c14e968c5987f7a
tiaxter <jerrytiapalmiotto@gmail.com>
2023-02-04 14:07:36 +0100

    feat: std::io readline example

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    io_example();
+}
+
+fn io_example() {
+    // Ask to uer to type something
+    println!("Type your name: ");
+    // Create new mutable variable
+    let mut name = String::new();
+    // Read the input from stdin
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("String has not been inserted");
+    // Print the given string
+    println!("Hello {}", name);
+}
